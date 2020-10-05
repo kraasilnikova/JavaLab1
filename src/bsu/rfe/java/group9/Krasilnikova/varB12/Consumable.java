6392e3519a12d59b8b60d4f6041de842e9af5c15
kraasilnikova <krasilnikova-11@mail.ru>
2020-10-05 11:27:28 +0300

    added package

---
+++ src/bsu/rfe/java/group9/Krasilnikova/varB12/Consumable.java
@@ -0,0 +1,5 @@
+package bsu.rfe.java.group9.Krasilnikova.varB12;
+
+public interface Consumable {
+    public abstract void consume();
+}
---
+++ src/bsu/rfe/java/group9/Krasilnikova/varB12/MainApplication.java
@@ -0,0 +1,4 @@
+package bsu.rfe.java.group9.Krasilnikova.varB12;
+
+public class MainApplication {
+}
