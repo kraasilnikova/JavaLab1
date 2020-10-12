5f5570bbee4c6a91ae8ddaccfd61ed616f9cf112
kraasilnikova <krasilnikova-11@mail.ru>
2020-10-12 10:14:41 +0300

    added Food Comparator

---
+++ src/bsu/rfe/java/group9/Krasilnikova/varB12/FoodComparator.java
@@ -0,0 +1,15 @@
+package bsu.rfe.java.group9.Krasilnikova.varB12;
+
+import java.util.Comparator;
+public class FoodComparator implements Comparator<Food>
+{
+    public int compare(Food arg0, Food arg1)
+    {
+        // если 1-ый объект = null, то он всегда "больше", т.е. перемещается в конец массива
+        if (arg0==null) return 1;
+        // если 2-ой объект = null, а 1-ый - нет (не сработала предыдущая строчка), то 1-ый всегда меньше, т.е. перемешается в начало массива
+        if (arg1==null) return -1;
+        // если оба объекта не null, то результат сравнения определяется сравнением их name
+        return arg0.getName().compareTo(arg1.getName());
+    }
+}
