6cbe1dca2c8f5055fed83141879ecfc2322597d7
kraasilnikova <krasilnikova-11@mail.ru>
2020-10-12 10:25:16 +0300

    added class Cheese

---
+++ src/bsu/rfe/java/group9/Krasilnikova/varB12/Cheese.java
@@ -0,0 +1,11 @@
+package bsu.rfe.java.group9.Krasilnikova.varB12;
+
+public class Cheese extends Food {
+    public Cheese()
+    {
+        super("Сыр");
+    }
+    public void consume() {
+        System.out.println(this + " съеден");
+    }
+}
