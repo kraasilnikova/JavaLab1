fa6c098f9af8b336a013f810f6e36ba9c54c878c
kraasilnikova <krasilnikova-11@mail.ru>
2020-10-12 11:32:11 +0300

    added calculate calories method

--- src/bsu/rfe/java/group9/Krasilnikova/varB12/Cheese.java
+++ src/bsu/rfe/java/group9/Krasilnikova/varB12/Cheese.java
@@ -1,11 +1,23 @@
 package bsu.rfe.java.group9.Krasilnikova.varB12;
 
-public class Cheese extends Food {
+public class Cheese extends Food
+{
+    Double calories = null;
     public Cheese()
     {
         super("Сыр");
     }
     public void consume() {
         System.out.println(this + " съеден");
+    }
+    public Double CalculateCalories()
+    {
+        if(this.name == "Сыр")
+        {
+            calories = 30.0;
+            return calories;
+        }
+        else
+            return 0.0;
     }
 }
